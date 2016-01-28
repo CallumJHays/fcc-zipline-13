e229e5be5768d1eb1e76dd64e78f71ac3a70998c
callumjhays <callumjhays@gmail.com>
2016-01-28 12:04:48 +0000

    first working version

--- server.js
+++ server.js
@@ -1,31 +1,29 @@
 'use strict';
 
 var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
+var request = require('request');
 
 var app = express();
-require('dotenv').load();
-require('./app/config/passport')(passport);
+var urlcount = 0;
 
-mongoose.connect(process.env.MONGO_URI);
-
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
-
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-routes(app, passport);
+app.get('/new/:url([^ \?]+$)/', function(req, res){
+	request.get(req.params.url, function(error, result, body){
+		if(error && req.query.allow !== 'true'){ // url invalid and invalidallow not true
+			return res.send({ERROR: 'provided url was invalid!'});
+		}
+		
+		var newUrl = '/' + urlcount;
+		urlcount++;
+		app.get(newUrl, function(inreq, inres){
+			inres.redirect(req.params.url);
+		});
+		
+		res.send({
+			original_url: req.params.url,
+			short_url: req.get('host') + newUrl
+		});
+	});
+});
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
